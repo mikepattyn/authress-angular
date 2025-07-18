90ccb6c3b07795556897041d7fcca93afa0cecbe
Mike Pattyn <mike@methylium.com>
2025-07-18 22:06:44 +0200

    idk

--- projects/angular/src/lib/authress.module.ts
+++ projects/angular/src/lib/authress.module.ts
@@ -1,6 +1,7 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
 import { LoginClient, Settings } from '@authress/login';
 
+export const LOGIN_CLIENT = new InjectionToken<LoginClient>("LoginClient");
 @NgModule()
 export class AuthressModule {
   static forRoot(config: Settings): ModuleWithProviders<AuthressModule> {
@@ -8,8 +9,15 @@ export class AuthressModule {
       ngModule: AuthressModule,
       providers: [
         {
+          provide: LOGIN_CLIENT,
+          useFactory: () => {
+            return new LoginClient(config);
+          },
+        },
+        // Also provide it as the original LoginClient for backward compatibility
+        {
           provide: LoginClient,
-          useFactory: () => new LoginClient(config),
+          useExisting: LOGIN_CLIENT,
         },
       ],
     };
